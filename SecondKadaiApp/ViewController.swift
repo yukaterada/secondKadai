b3b1d7d15f063675c486d9a45b5499e1398e372f
yukaterada-lvgs <yuka.terada@leverages.jp>
2020-02-23 22:22:02 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 寺田　優佳 on 2020/02/23.
+//  Copyright © 2020 yuuka.terada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
